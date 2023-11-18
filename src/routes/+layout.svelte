7cd4c0000f95693b600321d7d8d134a58585b3f9
Adrian Montoya <montoya@astralta.com>
2023-11-18 17:59:19 +0100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import '../app.postcss';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	import { AppShell } from '@skeletonlabs/skeleton';
+
+
+</script>
+
+
+
+<AppShell class="bg-[#EFEFEF] text-black font-sans" slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64">
+	<div class="w-full h-12 bg-[#478D61] altashadow justify-center flex flex-row fixed top-0">
+		<div class="justify-center flex items-center w-10">
+			<img src="/light_aivery.png" alt="smol_aivery" class="">
+		</div>
+		<div class="justify-center flex items-center uppercase text-3xl font-semibold text-white">
+			Aivery
+		</div>
+	</div>
+
+	<div class="w-full h-full flex mt-24">
+
+		<slot/>
+	</div>
+
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,72 @@
+<div class="w-full h-screen  flex flex-col lg:mx-60 px-4">
+	<div class="text-3xl font-bold">
+		Smart Outbound
+	</div>
+
+	<!-- Lead Selection -->
+	<div class="w-full h-20 pb-20 pt-16 flex justify-center flex-col">
+		<div class="py-2">
+			<span class="text-xl p-4 font-light">
+				Select Lead
+			</span>
+		</div>
+		<div>
+			<input type="text" name="lead" placeholder="Type in LinkdIn URL here" class="w-full h-10 placeholder:italic placeholder:text-opacity-60 rounded-md border-none bg-[#D9D9D9]">
+		</div>
+	</div>
+
+	<!-- Customisation -->
+	<div class="w-full h-20 py-20 flex justify-center flex-col">
+		<div class="py-2">
+			<span class="text-xl p-4 font-light">
+				Customise your message
+			</span>
+		</div>
+		<div>
+			<textarea name="bi" id="bi" cols="30" rows="10" class="w-full h-40 placeholder:italic placeholder:text-opacity-60 rounded-md border-none bg-[#D9D9D9] p-4">
+				
+
+			</textarea>
+		
+		</div>
+	</div>
+
+	<!-- Set the tone -->
+	<div class="w-full h-20 py-24 flex justify-center flex-col overflow-auto">
+		<div class="py-2">
+			<span class="text-xl p-4 font-light">
+				Set the tone
+			</span>
+		</div>
+		<div class="flex justify-start gap-3 flex-row">
+			<button class="bg-[#D9D9D9] rounded-md px-4 py-2 hover:px-8 hover:bg-[#478D61] transition-all duration-300 ease-in-out hover:text-white">
+				👔 Professional
+			</button>
+			<button class="bg-[#D9D9D9] rounded-md px-4 py-2 hover:px-8 hover:bg-[#478D61] transition-all duration-300 ease-in-out hover:text-white">
+				🙏 Friendly
+			</button>
+			<button class="bg-[#D9D9D9] rounded-md px-4 py-2 hover:px-8 hover:bg-[#478D61] transition-all duration-300 ease-in-out hover:text-white">
+				🤝 Salesman
+			</button>
+			<button class="bg-[#D9D9D9] rounded-md px-4 py-2 hover:px-8 hover:bg-[#478D61] transition-all duration-300 ease-in-out hover:text-white">
+				🐷 Ur mom
+			</button>
+		</div>
+	</div>
+
+	<!-- Result -->
+	<div class="w-full h-20 py-24 flex justify-center flex-col">
+		<div class="py-2">
+			<span class="text-xl p-4 font-light">
+				Result
+			</span>
+		</div>
+		<div>
+			<textarea name="bi" id="bi" cols="30" rows="10" class="w-full h-40 placeholder:italic placeholder:text-opacity-60 rounded-md border-none bg-[#D9D9D9] p-4">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+
+			</textarea>
+		
+		</div>
+	</div>
+</div>
